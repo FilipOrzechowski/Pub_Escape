a714695e08720e85086b72c35c144846b73be252
Filip <23342492+FilipOrzechowski@users.noreply.github.com>
2023-03-07 12:13:04 +0100

    Added distance estimation method and a_star algorithm

--- algorithm.py
+++ algorithm.py
@@ -10,3 +10,34 @@ def get_min_distance(distance_and_node_list):
   distance_and_node_list.remove(distance_and_node_list[index])
   return min_distance, node
 
+
+def heuristic(start, target):
+  x_distance = abs(start[0] - target[0])
+  y_distance = abs(start[1] - target[1])
+  return (x_distance**2 + y_distance**2)**.5
+
+
+def a_star(graph, start, target):
+  paths_and_distances = {}
+  for node in graph:
+    paths_and_distances[node] = [float('inf'), [start]]
+
+  paths_and_distances[start][0] = 0
+  nodes_to_explore = [(0, start)]
+  while nodes_to_explore and paths_and_distances[target][0] == float('inf'):
+    current_distance, current_node = get_min_distance(nodes_to_explore)
+
+    for neighbor in graph[current_node]:
+      new_distance = current_distance + 1 + heuristic(neighbor, target)
+      temp = paths_and_distances[current_node][1]
+      new_path = temp+[neighbor]
+      if new_distance < paths_and_distances[neighbor][0]:
+        paths_and_distances[neighbor][0] = new_distance
+        paths_and_distances[neighbor][1] = new_path
+        nodes_to_explore.append((new_distance, neighbor))
+        
+  if paths_and_distances[target][0] != float('inf'):
+    return paths_and_distances[target][1]
+  
+  else:
+    return None
